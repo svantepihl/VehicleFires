7000c62ef88cfcaa04af2486e7336719050945c5
Hyasurpolo <olivettileonardo@gmail.com>
2020-01-29 21:02:57 +0100

    added non arson-graphs, improved graphs for months and quarters, added graphs for single län

--- visualisations.R
+++ visualisations.R
@@ -1,9 +1,4 @@
-##### Anlagda Bilbränder, riket######
-
-Arson <- filter(dat_msb, dat_msb$Reason == "Arson")
-
-#Create labels for the months
-labels <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+######## Alla Bränder #########
 
 # yearly, comparision betweeen different causes 
 
@@ -11,25 +6,73 @@ labels <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"
 ggplot() + 
   geom_freqpoly(data = Arson, aes(x=year, colour ="red"), binwidth = 1) + 
   geom_freqpoly(data = Technical_malfunctioning, aes(x=year, colour = "blue"), binwidth=1) + geom_freqpoly(data = Others, aes(x=year, colour = "green"), binwidth=1) + geom_freqpoly(data = Unknown, aes(x=year, colour = "black"), binwidth=1)+
-  theme(legend.position="right")  + scale_colour_identity(guide = "legend", name= "cause",  labels= c("unknown","technical malfunctioning", "other Reason","arson" )) + labs(y = "amount") + xlim (1998, 2018) + guides(color = guide_legend(reverse = TRUE))
+  theme(legend.position="right")  + scale_colour_identity(guide = "legend", name= "cause",  labels= c("unknown","technical malfunctioning", "other reason","arson" )) + labs(y = "amount") + xlim (1998, 2018) + guides(color = guide_legend(reverse = TRUE))
+
 
 #Option 2
 ggplot(dat_msb, binwidth=1, aes(x=year, colour = Reason)) + geom_freqpoly (binwidth = 1) + xlim (1998, 2018) + labs (y="amount")
 
 
+
 #yearly, riket
-anlagda %>% 
+
+dat_msb %>% 
   ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
 
 #quarterly, riket
-anlagda %>% 
-  ggplot(aes(quarter)) + geom_freqpoly(binwidth = 1) + xlim (1,4) + labs(y = "amount")
+dat_msb %>% 
+  ggplot(aes(quarter)) + geom_bar(aes(fill = as.factor(quarter))) + labs(y = "amount") + scale_fill_discrete(name = "Quarters", labels = c("Jan - Mar", "Apr - Jun", "Jul - Sep", "Oct - Dec"))
+
 
 #monthly, riket
-breaks = seq(1, 12, by=1)
-anlagda %>% 
-  ggplot(aes(month)) + geom_freqpoly(binwidth = 1) + scale_x_continuous(limits = c(1, 12), breaks=breaks, labels = labels) + labs(y = "amount")
 
+#Create labels and breaks for the months
+labels <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+breaks = seq(1, 12, by=1)
+
+# Option 1
+dat_msb %>% 
+  ggplot(aes(month))+ geom_freqpoly(binwidth = 1) + scale_x_continuous(limits = c(1, 12), breaks=breaks, labels = labels) + labs(y = "amount")
+
+# Option 2
+dat_msb %>% 
+  ggplot(aes(month))+ geom_bar(aes(fill = as.factor(month))) + 
+  scale_x_continuous(breaks=breaks, labels = labels) + 
+  labs(y = "amount") + scale_fill_discrete(name = "Month", labels = labels)
+
+
+
+##### Anlagda Bilbränder, riket######
+
+Arson <- filter(dat_msb, dat_msb$Reason == "Arson")
+
+#yearly, riket
+
+Arson %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+#quarterly, riket
+Arson %>% 
+  ggplot(aes(quarter)) + geom_bar(aes(fill = as.factor(quarter))) + labs(y = "amount") + scale_fill_discrete(name = "Quarters", labels = c("Jan - Mar", "Apr - Jun", "Jul - Sep", "Oct - Dec"))
+  
+
+#monthly, riket
+
+#Create labels and breaks for the months
+labels <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+breaks = seq(1, 12, by=1)
+
+# Option 1
+Arson %>% 
+  ggplot(aes(month))+ geom_freqpoly(binwidth = 1) + scale_x_continuous(limits = c(1, 12), breaks=breaks, labels = labels) + labs(y = "amount")
+
+# Option 2
+Arson %>% 
+  ggplot(aes(month))+ geom_bar(aes(fill = as.factor(month))) + 
+  scale_x_continuous(breaks=breaks, labels = labels) + 
+  labs(y = "amount") + scale_fill_discrete(name = "Month", labels = labels)
 
 # yearly, all län at once!!
 
@@ -38,5 +81,65 @@ ggplot(Arson, binwidth=1, aes(x=year, colour = län)) + geom_freqpoly (binwidth 
 # yearly, län by län
 Blekinge %>% 
   ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
-# etc.etc.etc.
+
+Dalarna %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Gotland %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Gävleborg %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Halland %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Jämtland %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Jönköping %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Kalmar %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Kronoberg %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Norrbotten %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Skåne %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Stockholm %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Södermalm %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Uppsala %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Värmland %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Västerbotten %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Västernorrland %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Västmanland %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Västra_Götland %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Örebro %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
+Östergöta %>% 
+  ggplot(aes(year)) + geom_freqpoly(binwidth = 1) + xlim (1998, 2018) + labs(y = "amount")
+
 
