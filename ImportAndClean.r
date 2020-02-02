9cb47fbf5d47039614afe482ba238ec1e44d1247
Hyasurpolo <olivettileonardo@gmail.com>
2020-02-02 22:43:07 +0100

    added variable weekday

--- ImportAndClean.r
+++ ImportAndClean.r
@@ -16,8 +16,10 @@ dat_msb$year <- year(dat_msb$datum)
 dat_msb$quarter <- quarter(dat_msb$datum)
 dat_msb$month <- month(dat_msb$datum)
 dat_msb$day <- day(dat_msb$datum)
+dat_msb$weekday <- weekdays(dat_msb$datum)
 dat_msb$hour <- hour(dat_msb$tid)
 dat_msb$minute <- minute(dat_msb$tid)
+
 
 # We rename the Reasons behind the carfires to english
 dat_msb$Reason [dat_msb$Reason != "Fel i utrustning"& dat_msb$Reason != "Avsiktlig brand"& dat_msb$Reason != "Okänd"] <-"Other Reason"
@@ -30,8 +32,6 @@ Arson <- filter(dat_msb, dat_msb$Reason == "Arson")
 Technical_malfunctioning <- filter(dat_msb, dat_msb$Reason == "Technical Malfunctioning")
 Unknown <- filter(dat_msb, dat_msb$Reason == "Unknown")
 Others <- filter(dat_msb, dat_msb$Reason != "Arson", dat_msb$Reason != "Technical Malfunctioning", dat_msb$Reason != "Unknown")
-
-
 
 
 ######### Anlagda Bilbränder  ############
