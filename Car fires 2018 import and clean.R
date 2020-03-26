ab95b293b160deb260da2eee2e6e9ce976c65fb5
Hyasurpolo <olivettileonardo@gmail.com>
2020-03-26 14:31:10 +0100

    data cleaning 2018 updated

--- Car fires 2018 import and clean.R
+++ Car fires 2018 import and clean.R
@@ -4,36 +4,32 @@ require(lubridate)
 
 dat_2018 <- read_excel("dat_msb_2018.xlsx")
 
-colnames(dat_2018) <- c("code", "date", "time", "originary_fire", "consequence_fire", 
-                        "län_code", "län_name","kommun_code", "kommun_name",
-                        "kommun-type_code", "kommun_type", "deso_code", 
-                        "type_of_environment", "environment", "number_of_cars", 
+colnames(dat_2018) <- c("Code", "Date", "Time", "Origin_of_Fire", "Consequence_of_Fire", 
+                        "Region_Code", "Region_Name","Municiplaity_Code", "Municipality_Name",
+                        "Municiplaity_Type_Code", "Municipality_Type", "Deso_Code", 
+                        "Type_of_Environment", "Environment", "Number_of_Cars", 
                         "number_of_buses", "number_of_trucks",
-                        "number_of_campers", "number_of_other_vehicles", 
-                        "number_of_agricultural_machines", "number_of_forestry_machines",
-                        "number_of_other_work_machines", "number_of_trains", "number_of_boats",
-                        "number_of_helicopters_and_planes", "number_of_other_appliances_or_machines",
-                        "type_of_object_catching_fire_first", "object_catching_fire_first",
-                        "type_of_source_of_fire_source", "fire_source", "cause") 
+                        "Number_of_Campers", "Number_of_Other_Vehicles", 
+                        "Number_of_Agricultural_Machines", "Number_of_Forestry_Machines",
+                        "Number_of_Other_Work_Machines", "Number_of_Trains", "Number_of_Boats",
+                        "Number_of_Helicopters_and_Planes", "Number_of_Other_Appliances_or_Machines",
+                        "Type_of_Object_Catching_Fire_First", "Object_Catching_Fire_First",
+                        "Type_of_Source_of_Fire_Source", "Fire_Source", "Reason") 
 
 
 #Data cleaning- create separate year, quarter, month, day, hour and minute variables. 
-dat_2018$year <- year(dat_2018$date)
-dat_2018$quarter <- quarter(dat_2018$date)
-dat_2018$month <- month(dat_2018$date)
-dat_2018$day <- day(dat_2018$date)
-dat_2018$hour <- hour(dat_2018$time)
-dat_2018$minute <- minute(dat_2018$time)
+dat_2018$Year <- year(dat_2018$Date)
+dat_2018$Quarter <- quarter(dat_2018$Date)
+dat_2018$Month <- month(dat_2018$Date)
+dat_2018$Day <- day(dat_2018$Date)
+dat_2018$Hour <- hour(dat_2018$Time)
+dat_2018$Minute <- minute(dat_2018$Time)
 
-# We rename the causes behind the carfires to english
-dat_2018$cause [dat_2018$cause != "Fel i utrustning"& dat_2018$cause != "Avsiktlig händelse"& dat_2018$cause != "Gick inte att bedöma"] <-"Other cause"
-dat_2018$cause [dat_2018$cause == "Avsiktlig händelse"] <- "Arson"
-dat_2018$cause [dat_2018$cause == "Fel i utrustning"] <- "Technical Malfunctioning"
-dat_2018$cause [dat_2018$cause == "Gick inte att bedöma"] <- "Unknown"
+# We rename the Reasons behind the carfires to english
+dat_2018$Reason [dat_2018$Reason != "Fel i utrustning"& dat_2018$Reason != "Avsiktlig händelse"& dat_2018$Reason != "Gick inte att bedöma"] <-"Other Reason"
+dat_2018$Reason [dat_2018$Reason == "Avsiktlig händelse"] <- "Arson"
+dat_2018$Reason [dat_2018$Reason == "Fel i utrustning"] <- "Technical Malfunctioning"
+dat_2018$Reason [dat_2018$Reason == "Gick inte att bedöma"] <- "Unknown"
 
-# subset: anlagd, fel i utrustning, Gick inte att bedöma, annat
-Arson_2018 <- filter(dat_2018, dat_2018$cause == "Arson")
-Technical_malfunctioning_2018 <- filter(dat_2018, dat_2018$cause == "Technical Malfunctioning")
-Unknown_2018 <- filter(dat_2018, dat_2018$cause == "Unknown")
-Others_2018 <- filter(dat_2018, dat_2018$cause != "Arson", dat_2018$cause != "Technical Malfunctioning", dat_2018$cause != "Unknown")
+
 
