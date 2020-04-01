fcbb599380fc0cfdd1def8c77f63defb7c4c36c0
Svante Pihl <hej@svantepihl.com>
2020-04-01 10:41:46 +0200

    commented out weather loading due to long compute time

--- weather.r
+++ weather.r
@@ -15,22 +15,22 @@ dat_weather_relevant <- subset(dat_weather, year(dat_weather$Date) >= 2012)
 
 
 # Add weather data to observations (takes a while to run)
-for(i in 1:nrow(dat_stockholm_relevant)) {
-  progress(i/(nrow(dat_stockholm_relevant)/100))
-  for(j in 1:nrow(dat_weather_relevant)) {
-    # check if correct municipality
-    if(dat_stockholm_relevant$Municipality_Code[i] == dat_weather_relevant$Municipality_Code[j]) {
-      if(dat_stockholm_relevant$Date[i] == dat_weather_relevant$Date[j]) {
-        dat_stockholm_relevant$Temperature[i] <- dat_weather_relevant$Temperature[j]
-        dat_stockholm_relevant$Precipitation[i] <- dat_weather_relevant$Precipitation[j]
-        break()
-      }
-    }
-  }
-}
+#for(i in 1:nrow(dat_stockholm_relevant)) {
+#  progress(i/(nrow(dat_stockholm_relevant)/100))
+#  for(j in 1:nrow(dat_weather_relevant)) {
+#    # check if correct municipality
+#    if(dat_stockholm_relevant$Municipality_Code[i] == dat_weather_relevant$Municipality_Code[j]) {
+#      if(dat_stockholm_relevant$Date[i] == dat_weather_relevant$Date[j]) {
+#        dat_stockholm_relevant$Temperature[i] <- dat_weather_relevant$Temperature[j]
+#        dat_stockholm_relevant$Precipitation[i] <- dat_weather_relevant$Precipitation[j]
+#        break()
+#      }
+#    }
+#  }
+#}
 
 write_csv(dat_stockholm_relevant, path = "exports/dat_after2012.csv")
 
-?write_csv
+
 
 
