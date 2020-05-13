0cfa162dbee2ff00841efbe554f46d698d7d300f
Hyasurpolo <58738029+Hyasurpolo@users.noreply.github.com>
2020-05-13 11:42:29 +0200

    odel continues

--- Models/Panel Data Models.R
+++ Models/Panel Data Models.R
@@ -17,27 +17,9 @@ require(sandwich)
 require(sjstats)
 
 
-#remove unsed variables
-dat_months_stockholm_lm <- dat_months_stockholm[ ,-c(2,4, 6:16,18,19,22)]
-
-# dat_months_stockholm_lm_impute <- dat_months_stockholm_lm[ , colSums(is.na(dat_months_stockholm_lm)) == 0] - coloumns with na values
-
-full.model <- lm(Number_of_Fires_Month ~.-1, data = dat_months_stockholm_lm)
-
-dat_months_stockholm_lm <- na.omit(dat_months_stockholm_lm)
-summary(full.model)
-
-
-step.model <- stepAIC(full.model, direction = "both", 
-                      trace = TRUE)
-
-summary(step.model)
-
-plot(step.model$residuals)
-
-
 # Dummy variables estimators, equivalent fixed effects
 model_months <- lm(formula = dat_months_stockholm$Number_of_Fires_Month ~ 
+                   + dat_months_stockholm$Municipality_Name
                    + dat_months_stockholm$Temperature
                    + dat_months_stockholm$Percentage_of_Unemployed_18_64
                    + dat_months_stockholm$Total_Number_of_Residents
@@ -62,6 +44,8 @@ model_months_3 <- glm.nb(formula = dat_months_stockholm$Number_of_Fires_Month ~
 summary(model_months)
 summary(model_months_2)
 summary(model_months_3)
+
+vif(model_months) #remember to take away muncipality names
 
 # Classic panel models" 
 
@@ -104,7 +88,6 @@ lmtest::coeftest(model_months_poisson_fixed, vcov. = sandwich::vcovHC(model_mont
 lmtest::coeftest(model_months_poisson_first_difference, vcov. = sandwich::vcovHC(model_months_plm_fixed, type = 'HC1'))
 
 
-
 model_months_neg_bin_random <- pglm(form, data = dat_months_stockholm, model = "random", family = "poisson", index = c("Municipality_Name","Date"))
 model_months_neg_bin_fixed <- pglm(form, data = dat_months_stockholm, model = "within", family= "poisson", index = c("Municipality_Name","Date"))
 model_months_neg_bin_pooled <- pglm(form, data = dat_months_stockholm, model = "pooling", family= "poisson", index = c("Municipality_Name", "Date"))
@@ -123,7 +106,7 @@ phtest(model_months_plm_random, model_months_plm_fixed)
 
 phtest(model_days_plm_random, model_days_plm_fixed)
 
-vif(model_months_plm_fixed)
+
 
 
 
