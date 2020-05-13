03a3b70b28aa1d87266e7179efef1cbeb42e77cf
Hyasurpolo <58738029+Hyasurpolo@users.noreply.github.com>
2020-05-13 11:46:58 +0200

    mini fix models

--- Models/Panel Data Models.R
+++ Models/Panel Data Models.R
@@ -34,7 +34,8 @@ model_months_poi <- glm(formula = dat_months_stockholm$Number_of_Fires_Month ~
                         dat_months_stockholm$Municipality_Name 
                       + dat_months_stockholm$Temperature 
                       + dat_months_stockholm$Percentage_of_Unemployed_18_64
-                      +dat_months_stockholm$Total_Number_of_Residents-1, family = "poisson")
+                      +dat_months_stockholm$Total_Number_of_Residents
+                      + dat_months_stockholm$Median_Income_20plus -1, family = "poisson")
 
 # Summary with robust errors
 lmtest::coeftest(model_months_poi, vcov. = sandwich::vcovHC(model_months_poi, type = 'HC1'))
@@ -43,7 +44,8 @@ model_months_nb <- glm.nb(formula = dat_months_stockholm$Number_of_Fires_Month ~
                            dat_months_stockholm$Municipality_Name 
                          + dat_months_stockholm$Temperature 
                          + dat_months_stockholm$Percentage_of_Unemployed_18_64
-                         +dat_months_stockholm$Total_Number_of_Residents -1)
+                         +dat_months_stockholm$Total_Number_of_Residents
+                         + dat_months_stockholm$Median_Income_20plus -1)
 
 # Summary with robust errors
 lmtest::coeftest(model_months_nb, vcov. = sandwich::vcovHC(model_months_nb, type = 'HC1'))
