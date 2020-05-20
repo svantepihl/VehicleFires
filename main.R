c1207b491f52d44e350e9e8683d38ce8b11006e0
Svante Pihl <hej@svantepihl.com>
2020-05-20 09:26:06 +0200

    small fix in main.r for subsets to work

--- main.R
+++ main.R
@@ -1,7 +1,9 @@
+require(readr)
+require(zoo)
+require(lubridate)
 #for Leo's computer
 #getOption("encoding")
 #options(encoding = "ISO-8859-1")
-
 #Sys.setlocale(category = "LC_ALL", locale = "Swedish")
 
 ##### IMPORT NOTE IT TAKES 5-15 MINUTES TO RUN #####
@@ -9,8 +11,8 @@ if(file.exists("DATA/dat_months_stockholm_all.csv")){
   dat_months_stockholm_all <- read_csv("DATA/dat_months_stockholm_all.csv")
   dat_months_stockholm_all$Date <- as.yearmon(dat_months_stockholm_all$Date)
   
-  dat_months_stockholm_2018 <- subset(dat_months_stockholm_all,dat_months_stockholm_all$Year == 2018)
-  dat_months_stockholm <- subset(dat_months_stockholm_all,dat_months_stockholm_all$Year != 2018)
+  dat_months_stockholm_2018 <- subset(dat_months_stockholm_all, year(dat_months_stockholm_all$Date) == 2018)
+  dat_months_stockholm <- subset(dat_months_stockholm_all, year(dat_months_stockholm_all$Date) != 2018)
 } else {
   print("LOADING DATA DURIATION APPROX 15 MIN")
   source("ImportCleanWrangle.R")
